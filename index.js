6220c4837baefd5e6843aa602fda4b2e2da54e68
Jeff Jewiss <jeffjewiss@gmail.com>
2015-08-11 14:00:01 -0400

    Changes plugin to work as a filter
    Operates on all files in the inputNode versus having to specify input
    and otuput files as options.

--- index.js
+++ index.js
@@ -1,63 +1,48 @@
-var path = require('path');
-var fs = require('fs');
-var mkdirp = require('mkdirp');
-var assign = require("object-assign");
-var CachingWriter = require('broccoli-caching-writer');
+var Filter = require('broccoli-filter');
+var assign = require('object-assign');
 var postcss = require('postcss');
 var CssSyntaxError = require('postcss/lib/css-syntax-error');
 
-function PostcssCompiler (inputTrees, inputFile, outputFile, plugins, map) {
-    if ( !(this instanceof PostcssCompiler) ) {
-        return new PostcssCompiler(inputTrees, inputFile, outputFile, plugins, map);
+function PostcssFilter(inputNode, options) {
+    if ( !(this instanceof PostcssFilter) ) {
+        return new PostcssFilter(inputNode, options);
     }
 
-    if ( !Array.isArray(inputTrees) ) {
-        throw new Error('Expected array for first argument - did you mean [tree] instead of tree?');
-    }
-
-    CachingWriter.call(this, inputTrees, inputFile);
-
-    this.inputFile = inputFile;
-    this.outputFile = outputFile;
-    this.plugins = plugins || [];
-    this.map = map || {};
     this.warningStream = process.stderr;
+    this.options = options || {};
+    Filter.call(this, inputNode);
 }
 
-PostcssCompiler.prototype = Object.create(CachingWriter.prototype);
-PostcssCompiler.prototype.constructor = PostcssCompiler;
+PostcssFilter.prototype = Object.create(Filter.prototype);
+PostcssFilter.prototype.constructor = PostcssFilter;
+PostcssFilter.prototype.extensions = ['css'];
+PostcssFilter.prototype.targetExtension = 'css';
 
-PostcssCompiler.prototype.updateCache = function (includePaths, destDir) {
-    var toFilePath = path.join(destDir, this.outputFile);
-    var fromFilePath = path.join(includePaths[0], this.inputFile);
+PostcssFilter.prototype.processString = function (str, relativePath) {
+    var opts = this.options;
 
-    if ( !this.plugins || this.plugins.length < 1 ) {
+    if ( !opts.plugins || opts.plugins.length < 1 ) {
         throw new Error('You must provide at least 1 plugin in the plugin array');
     }
 
     var processor = postcss();
-    var css = fs.readFileSync(fromFilePath, 'utf8');
-    var options = {
-        from: fromFilePath,
-        to: toFilePath,
-        map: this.map
-    };
-
-    this.plugins.forEach(function (plugin) {
-        var pluginOptions = assign(options, plugin.options || {});
-        processor.use(plugin.module(pluginOptions));
-    });
-
     var warningStream = this.warningStream;
 
-    return processor.process(css, options)
+    assign(opts, {
+        from: relativePath,
+        to: relativePath
+    });
+
+    opts.plugins.forEach(function (plugin) {
+        processor.use(plugin.module(assign(opts, plugin.options)));
+    });
+
+    return processor.process(str, opts)
         .then(function (result) {
             result.warnings().forEach(function (warn) {
                 warningStream.write(warn.toString());
             });
-
-            mkdirp.sync(path.dirname(toFilePath));
-            fs.writeFileSync(toFilePath, result.css);
+            return result.css;
         })
         .catch(function (error) {
             if ( 'CssSyntaxError' === error.name ) {
@@ -68,4 +53,4 @@ PostcssCompiler.prototype.updateCache = function (includePaths, destDir) {
         });
 };
 
-module.exports = PostcssCompiler;
+module.exports = PostcssFilter;
--- test.js
+++ test.js
@@ -8,35 +8,47 @@ var rimraf = require('rimraf');
 var glob = require('glob');
 var async = require('async');
 
-var basicPluginSet = [
-    {
-        module: require('postcss-pseudoelements')
-    }
-];
-
-var optionsPluginSet = [
-    {
-        module: require('postcss-pseudoelements'),
-        options: {
-            to: 'newOutput.css'
+var basicPluginSet = {
+    plugins: [
+        {
+            module: require('postcss-pseudoelements')
         }
+    ],
+    map: {
+        inline: false,
+        annotation: false
     }
-];
-
-var testWarnPluginSet = [
-    {
-        module: postcss.plugin('postcss-test-warn', function (opts) {
-            return function (css, result) {
-                result.warn("This is a warning.");
-            };
-        })
-    }
-];
-
-var map = {
-    inline: false,
-    annotation: false
 };
+
+var optionsPluginSet = {
+    to: 'newOutput.css',
+    plugins: [
+        {
+            module: require('postcss-pseudoelements')
+        }
+    ],
+    map: {
+        inline: false,
+        annotation: false
+    }
+};
+
+var testWarnPluginSet = {
+    plugins: [
+        {
+            module: postcss.plugin('postcss-test-warn', function (opts) {
+                return function (css, result) {
+                    result.warn("This is a warning.");
+                };
+            })
+        }
+    ],
+    map: {
+        inline: false,
+        annotation: false
+    }
+};
+
 
 var warnings = [];
 var warningStreamStub = {
@@ -57,29 +69,29 @@ afterEach(function () {
 });
 
 it('should process css', function () {
-    var outputTree = postcssCompiler(['fixture'], 'fixture.css', 'output.css', basicPluginSet, map);
+    var outputTree = postcssCompiler('fixtures/default', basicPluginSet);
     outputTree.warningStream = warningStreamStub;
 
     return (new broccoli.Builder(outputTree)).build().then(function (dir) {
-        var content = fs.readFileSync(path.join(dir.directory, 'output.css'), 'utf8');
+        var content = fs.readFileSync(path.join(dir.directory, 'fixture.css'), 'utf8');
         assert.strictEqual(content.trim(), 'a:before { content: "test"; }');
         assert.deepEqual(warnings, []);
     });
 });
 
 it('should expose warnings', function () {
-    var outputTree = postcssCompiler(['fixture'], 'warning.css', 'output.css', testWarnPluginSet, map);
+    var outputTree = postcssCompiler('fixtures/warning', testWarnPluginSet);
     outputTree.warningStream = warningStreamStub;
 
     return (new broccoli.Builder(outputTree)).build().then(function (dir) {
-        var content = fs.readFileSync(path.join(dir.directory, 'output.css'), 'utf8');
+        var content = fs.readFileSync(path.join(dir.directory, 'fixture.css'), 'utf8');
         assert.strictEqual(content.trim(), "a {}");
         assert.deepEqual(warnings, [ "postcss-test-warn: This is a warning." ]);
     });
 });
 
 it('should expose syntax errors', function () {
-    var outputTree = postcssCompiler(['fixture'], 'syntax-error.css', 'output.css', testWarnPluginSet, map);
+    var outputTree = postcssCompiler('fixtures/syntax-error', testWarnPluginSet);
     outputTree.warningStream = warningStreamStub;
 
     var count = 0;
@@ -96,7 +108,7 @@ it('should expose syntax errors', function () {
 });
 
 it('should expose non-syntax errors', function () {
-    var outputTree = postcssCompiler(['fixture'], 'missing-file.css', 'output.css', testWarnPluginSet, map);
+    var outputTree = postcssCompiler('fixture', testWarnPluginSet);
     outputTree.warningStream = warningStreamStub;
 
     var count = 0;
